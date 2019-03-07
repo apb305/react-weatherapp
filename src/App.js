93ebb6ef5cac69b13c42f65a4e0dcb6c6d87f1c0
Anthony Bernard <tony@anthonys-mbp.home>
2019-03-07 14:55:36 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,143 @@
+import React, { Component } from "react";
+import axios from "axios";
+import WeatherBox from "./components/weatherBox";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: "",
+      zipCode: "",
+      weatherPicture: "",
+      weatherDescription: "",
+      temperature: "",
+      humidity: "",
+      hideBody: true,
+      hideImage: false,
+      hideSpinner: false
+    };
+    this.handleUserInput = this.handleUserInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUserInput(event) {
+    event.preventDefault()
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${
+        this.state.zipCode
+      },us&units=metric&&APPID=${
+        process.env.REACT_APP_SECRET_TOKEN
+      }`
+    )
+    .then(res => {
+      //console.log(res.data);
+        const weatherImage = res.data.weather.map(items => {
+          return items.icon
+        })
+        const weatherDescription = res.data.weather.map(items => {
+          return items.description
+        })
+          this.setState({
+            location: res.data.name,
+            weatherPicture: weatherImage,
+            weatherDescription: `(${weatherDescription})`,
+            temperature: `Current Temp: ${Math.trunc((res.data.main.temp * 9) / 5 + 32)}°`,
+            humidity: `Humidity: ${res.data.main.humidity}%`,
+            hideBody: false,
+            hideSpinner: true,
+            hideImage: false
+          });
+    })
+    .catch(error => {
+      //console.log(error)
+      if (error) {
+        this.setState({
+          location: "Please check your zipcode."
+        })
+      }
+    })
+  }
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        //console.log(position)
+        if (position) {
+          axios
+            .get(
+              `https://api.openweathermap.org/data/2.5/weather?us&units=metric&lat=${
+              position.coords.latitude
+            }&lon=${position.coords.longitude}&APPID=${
+              process.env.REACT_APP_SECRET_TOKEN
+            }`
+            )
+            .then(res => {
+              //console.log(res.data);
+              const weatherImage = res.data.weather.map(items => {
+                return items.icon
+              })
+              const weatherDescription = res.data.weather.map(items => {
+                return items.description
+              })
+              this.setState({
+                location: res.data.name,
+                weatherPicture: weatherImage,
+                weatherDescription: `(${weatherDescription})`,
+                temperature: `Current Temp: ${Math.trunc((res.data.main.temp * 9) / 5 + 32)}°`,
+                humidity: `Humidity: ${res.data.main.humidity}%`,
+                hideBody: false,
+                hideSpinner: true
+              });
+            });
+        }
+      }, error => {
+        if (error) {
+          //console.log(error)
+          this.setState({
+            location: error.message,
+            hideBody: false,
+            hideSpinner: true,
+            hideImage: true
+          });
+        }
+      });
+    } else {
+      this.setState({
+        location: "Geolocation is not supported by this browser."
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="container text-center mt-3">
+        <WeatherBox
+          location={this.state.location}
+          weatherPicture={`https://openweathermap.org/img/w/${
+            this.state.weatherPicture
+          }.png`}
+          temperature={this.state.temperature}
+          humidity={this.state.humidity}
+          hideBody={this.state.hideBody}
+          weatherDescription={this.state.weatherDescription}
+          hideSpinner={this.state.hideSpinner}
+          zipCode={this.state.zipCode}
+          handleUserInput={this.handleUserInput}
+          handleSubmit={this.handleSubmit}
+          errorMessage={this.state.errorMessages}
+          hideImage={this.state.hideImage}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/weatherBox.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function weatherBox(props) {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 mx-auto mt-3">
+        <h5 className="card-title mt-3">ReactJS Weather App</h5>
+            <p>Written by Anthony Bernard Jr.</p>
+            <p>See code for this project<a href="https://github.com/apb305" target="noopener noreferrer"> here</a></p>
+          <div
+            className="card rounded-1 bg-light shadow"
+          >
+            <div className="text-center">
+              <div
+                hidden={props.hideSpinner}
+                className="spinner-border mb-3 mt-3"
+                role="status"
+              />
+            </div>
+            <div hidden={props.hideBody}>
+              <h5 className="card-text mt-3">{props.location}</h5>
+              <img hidden={props.hideImage}
+                src={props.weatherPicture}
+                style={{ width: "70px", height: "70px" }}
+                alt="Weather Img"
+              />
+              <p className="card-text">{props.weatherDescription}</p>
+              <p className="card-text">{props.temperature}</p>
+              <p className="card-text">{props.humidity}</p>
+              <form className="mt-2" onSubmit={props.handleSubmit}>
+                <input type="text" name="zipCode" value={props.zipCode} className="form-control-sm mb-4" onChange={props.handleUserInput} placeholder="Enter City or Zip"></input>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default weatherBox;
